6c7e58a0ab7ef8f68e79bddddf33fc6dae80931b
Jonathan Ngo <ngohuutrong.jonathan@gmail.com>
2023-09-18 23:51:57 +0700

    init commit

---
+++ dotnetcore_fluent_validation/Controllers/ExampleController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+#region Author
+//JonathanNgo
+#endregion
+
+namespace dotnetcore_fluent_validation.Controllers;
+
+[Route("api/[controller]")]
+public class ExampleController : Controller
+{
+    public ExampleController()
+    {
+        
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Nice!");
+    }
+}
---
+++ dotnetcore_fluent_validation/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMvcCore();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
+
